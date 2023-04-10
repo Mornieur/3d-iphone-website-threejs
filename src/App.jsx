36806d4ca0d7d3485e7f2467d573fa8f6104a9bc
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-10 14:39:39 -0300

    feat: add components into main apge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import WebgiViewer from "./components/WebgiViewer";
 
 function App() {
-
   return (
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
