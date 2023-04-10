987ae503af3621159ee28e0518ad8ebb3501cc06
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-10 14:41:42 -0300

    feat: create sound section component

---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,25 @@
+function SoundSection() {
+  return (
+    <div className="sound-section wrapper">
+      <div className="body">
+        <div className="sound-section-content content">
+          <h2 className="title">New Sound System</h2>
+          <p className="text"> Teel the base.</p>
+          <span className="description">
+            From $41.2/mo. for 24mo. or $999 before trade-in
+          </span>
+          <ul className="links">
+            <li>
+              <button className="button">Buy</button>
+            </li>
+            <li>
+              <a className="link">Learn More</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SoundSection;
