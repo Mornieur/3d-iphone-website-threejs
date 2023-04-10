c5f4395e771e0f626ec3121591005af94250898a
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-10 16:15:15 -0300

    feat: add webgi viewer variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import DisplaySection from "./components/DisplaySection";
-import Jumbotron from "./components/Jumbotron";
+import React, { useRef } from "react";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
 import WebgiViewer from "./components/WebgiViewer";
+import Loader from "./components/Loader";
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
   return (
     <div className="App">
-      <Nav />
-      <Jumbotron />
-      <SoundSection />
-      <DisplaySection />
-      <WebgiViewer />
+      <Loader />
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
-
 export default App;
